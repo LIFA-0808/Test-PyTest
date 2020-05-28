3da1f605cf34b352b2d0cce1cb97bed7ddff1a52
Lifanova <iramilka123@gmail.com>
2020-05-28 18:24:02 +0300

    corrected task

--- test_items.py
+++ test_items.py
@@ -6,11 +6,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_adding_button(browser):
     browser.get(link)
 
-    time.sleep(30)
+    # time.sleep(30)
 
-    button = len(browser.find_element_by_css_selector(".btn-add-to-basket").text)
-
-    assert button > 0, 'no button'
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), 'no button'
 
     # pytest --language=es test_items.py
     # pytest --language=fr test_items.py
