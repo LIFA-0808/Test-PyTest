b0736649df30f9aa2cbc441f1e1c6b8a7b906b10
Lifanova <iramilka123@gmail.com>
2020-05-30 14:33:12 +0300

    parametrize test_product_page - 1 xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,22 @@
-from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+import pytest
+
+link_list = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+             pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                          marks=pytest.mark.xfail),
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', link_list)
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_able_to_add_product_to_basket()
